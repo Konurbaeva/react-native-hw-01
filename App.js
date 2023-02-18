95532cf49cfe691b216244db6660d9f791b1db4a
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-18 13:07:55 +0100

    added ImageBackground and image

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RegistrationScreen from "./Screens/RegistrationScreen"
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Hello, Madina!</Text>
-      <Text>Hello, mobile!</Text>
+      <RegistrationScreen/>
       <StatusBar style="auto" />
     </View>
   );
