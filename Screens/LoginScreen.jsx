3d8d7c4e3cde366a3ba0ddacdf0c64bc75c5adca
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-20 19:56:51 +0100

    added Login screen form redundant with registration

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,31 +1,147 @@
 import React from 'react';
 import {
-    View,
-    Button,
-    ImageBackground,
-    TextInput,
-    Text,
-    TouchableOpacity,
-    Platform,
-    KeyboardAvoidingView,
-    TouchableWithoutFeedback,
-    Keyboard,
-    Dimensions,
-    Image
-  } from "react-native";
+  View,
+  ImageBackground,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Dimensions,
+  Image,
+  Button
+} from "react-native";
+import { useState, useEffect } from "react";
+import * as Font from "expo-font";
+// import { AppLoading } from "expo";
+import AppLoading from 'expo-app-loading';
 
-  import { globalStyles } from '../config/globalStyles';
+import { globalStyles } from '../config/globalStyles';
+
+const initialState = {
+  email: '',
+  password:''
+}
+
+const loadApplication = async() => {
+  await Font.loadAsync({
+    'DMMono-Regular': require('../assets/fonts/DMMono-Regular.ttf')
+  })
+}
 
 
-  export default function LoginScreen({navigation}) {
-    return (
-        <View style={ globalStyles.container}>    
-         <Text> Login Screen</Text>
-         <Button
-        title="Нет аккаунта? Зарегистрироваться"
-        onPress={() => navigation.navigate('Register')}
+//   export default function LoginScreen({navigation}) {
+//     return (
+//         <View style={ globalStyles.container}>    
+//          <Text> Login Screen</Text>
+//          <Button
+//         title="Нет аккаунта? Зарегистрироваться"
+//         onPress={() => navigation.navigate('Register')}
       
-      />
+//       />
+//         </View>
+//       );
+//   }
+
+export default function LoginScreen({navigation}) {
+    const [isShowKeyboard, setIsShowKeyboard] = useState(false)
+    const [state, setState] = useState(initialState)
+    const [isReady, setIsReady] = useState(false)
+    
+    const [dimensions, setDimensions] = useState(Dimensions.get("window").width-20*2)
+    
+     const keyboardHide = () => {
+      setIsShowKeyboard(false)
+      Keyboard.dismiss()
+      console.log(state)
+      setState(initialState)
+     }
+    
+     useEffect(() => {
+     const onChange= () => {
+      const width = Dimensions.get("window").width
+      setDimensions(width)
+     }
+     dimensionsHandler = Dimensions.addEventListener('change', onChange)
+    
+    return ()=> dimensionsHandler.remove()
+     }, [])
+    
+     if (!isReady) {
+      return (
+        <AppLoading
+          startAsync={loadApplication}
+          onFinish={() => setIsReady(true)}
+          onError={console.warn}
+        />
+      );
+    }
+      return (
+        <TouchableWithoutFeedback onPress={keyboardHide}>
+        <View style={ globalStyles.container}>
+         
+          <ImageBackground
+            style={ globalStyles.image}
+            source={require("../assets/PhotoBG.png")}>
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+              <Image  style={ globalStyles.imageRectangle} source={require("../assets/Rectangle.png")}/>
+             <Image width={200} height={200} style={ globalStyles.imageWhite} source={require("../assets/BG.png")}/>
+             <Image
+             style={ globalStyles.imagePlusIcon} 
+            source={{
+              uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+            }}
+          />
+            <View style={{... globalStyles.form, marginBottom: isShowKeyboard? 20 : 100, width: dimensions}}>
+              <View style={ globalStyles.header}> 
+              <Text style={ globalStyles.headerTitle}>Регистрация</Text>
+              </View>
+              <View>
+                <Text style={ globalStyles.inputTitle} 
+                onFocus={() => setIsShowKeyboard(true)}
+                >Email address:</Text>
+                <TextInput style={ globalStyles.input} textAlign={"center"} 
+                 placeholder="Адрес электронной почты"
+                 value={state.email}
+                 onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, email: value }))
+                }
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={ globalStyles.inputTitle}>Password:</Text>
+                <TextInput
+                  style={ globalStyles.input}
+                  textAlign={"center"}
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  placeholder="Пароль"
+                  value={state.password}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, password: value }))
+                  }
+                />
+              
+              </View>
+              <TouchableOpacity activeOpacity={0.7} style={ globalStyles.button} 
+              onPress={() => setIsShowKeyboard(false)}
+              >
+                <Text style={ globalStyles.buttonTitle}>Войти</Text>
+               
+              </TouchableOpacity>
+  
+              <Button
+          title="Нет аккаунта? Зарегистрироваться"
+          //onPress={() => navigation.navigate('Login')}
+          onPress={() => navigation.navigate('Register')}
+        />
+            </View>
+            </KeyboardAvoidingView>
+          </ImageBackground>
+        
         </View>
+        </TouchableWithoutFeedback>
       );
   }
