ad29c79cff35aff322b766de5b6f16d8555276d4
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-21 15:17:46 +0100

    added Prettier formatting

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -11,10 +11,10 @@ import {
   Keyboard,
   Dimensions,
   Image,
-  Button
-} from "react-native";
-import { useState, useEffect } from "react";
-import * as Font from "expo-font";
+  Button,
+} from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Font from 'expo-font';
 // import { AppLoading } from "expo";
 import AppLoading from 'expo-app-loading';
 
@@ -23,48 +23,46 @@ import colors from '../config/colors';
 
 const initialState = {
   email: '',
-  password:''
-}
+  password: '',
+};
 
-const loadApplication = async() => {
+const loadApplication = async () => {
   await Font.loadAsync({
-    'DMMono-Regular': require('../assets/fonts/DMMono-Regular.ttf')
-  })
-}
+    'DMMono-Regular': require('../assets/fonts/DMMono-Regular.ttf'),
+  });
+};
 
 export default function RegistrationScreen({ navigation }) {
-  const [isShowKeyboard, setIsShowKeyboard] = useState(false)
-  const [state, setState] = useState(initialState)
-  const [isReady, setIsReady] = useState(false)
-  
-  const [dimensions, setDimensions] = useState(
-    Dimensions.get("window").width - 20 * 2
-  );
-  
-   const keyboardHide = () => {
-    setIsShowKeyboard(false)
-    Keyboard.dismiss()
-    console.log(state)
-    setState(initialState)
-   }
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [isReady, setIsReady] = useState(false);
 
-   useEffect(() => {
-  //  const onChange= () => {
-  //   const width = Dimensions.get("window").width
-  //   setDimensions(width)
-  //  }
+  const [dimensions, setDimensions] = useState(Dimensions.get('window').width - 20 * 2);
 
-   const onChange = () => {
-    const width = Dimensions.get("window").width - 20 * 2;
-
-    setDimensions(width);
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
   };
-   dimensionsHandler = Dimensions.addEventListener('change', onChange)
-  
-  return ()=> dimensionsHandler.remove()
-   }, [])
-  
-   if (!isReady) {
+
+  useEffect(() => {
+    //  const onChange= () => {
+    //   const width = Dimensions.get("window").width
+    //   setDimensions(width)
+    //  }
+
+    const onChange = () => {
+      const width = Dimensions.get('window').width - 20 * 2;
+
+      setDimensions(width);
+    };
+    dimensionsHandler = Dimensions.addEventListener('change', onChange);
+
+    return () => dimensionsHandler.remove();
+  }, []);
+
+  if (!isReady) {
     return (
       <AppLoading
         startAsync={loadApplication}
@@ -73,76 +71,78 @@ export default function RegistrationScreen({ navigation }) {
       />
     );
   }
-    return (
-      <TouchableWithoutFeedback onPress={keyboardHide}>
-      <View style={ globalStyles.container}>
-       
-        <ImageBackground
-          style={ globalStyles.image}
-          source={require("../assets/PhotoBG.png")}>
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={globalStyles.container}>
+        <ImageBackground style={globalStyles.image} source={require('../assets/PhotoBG.png')}>
           <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <Image  style={ globalStyles.imageRectangle} source={require("../assets/Rectangle.png")}/>
-           <Image width={200} height={200} style={ globalStyles.imageWhite} source={require("../assets/BG.png")}/>
-           <Image
-           style={ globalStyles.imagePlusIcon} 
-          source={{
-            uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-          }}
-        />
-          <View style={{... globalStyles.form, marginBottom: isShowKeyboard? 20 : 150, width: dimensions}}>
-            <View style={ globalStyles.header}> 
-            <Text style={ globalStyles.headerTitle}>Регистрация</Text>
-            </View>
-            <View>
-              <Text style={ globalStyles.inputTitle} 
-              onFocus={() => setIsShowKeyboard(true)}
-              >Email address:</Text>
-              <TextInput 
-             style={ globalStyles.input}
-             // style={[globalStyles.input, isActive && { backgroundColor: 'purple' }]}
-               textAlign={"center"} 
-               placeholder="Адрес электронной почты"
-               value={state.email}
-               onChangeText={(value) =>
-                setState((prevState) => ({ ...prevState, email: value }))
-              }
-
-
-              />
-            </View>
-            <View style={{ marginTop: 20 }}>
-              <Text style={ globalStyles.inputTitle}>Password:</Text>
-              <TextInput
-                style={ globalStyles.input}
-                textAlign={"center"}
-                secureTextEntry={true}
-                onFocus={() => setIsShowKeyboard(true)}
-                placeholder="Пароль"
-                value={state.password}
-                onChangeText={(value) =>
-                  setState((prevState) => ({ ...prevState, password: value }))
-                }
-              />
-            
-            </View>
-            <TouchableOpacity activeOpacity={0.7} style={ globalStyles.button} 
-            onPress={() => setIsShowKeyboard(false)}
+            <Image
+              style={globalStyles.imageRectangle}
+              source={require('../assets/Rectangle.png')}
+            />
+            <Image
+              width={200}
+              height={200}
+              style={globalStyles.imageWhite}
+              source={require('../assets/BG.png')}
+            />
+            <Image
+              style={globalStyles.imagePlusIcon}
+              source={{
+                uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+              }}
+            />
+            <View
+              style={{
+                ...globalStyles.form,
+                marginBottom: isShowKeyboard ? 20 : 150,
+                width: dimensions,
+              }}
             >
-              <Text style={ globalStyles.buttonTitle}>Зарегистрироваться</Text>
-             
-            </TouchableOpacity>
+              <View style={globalStyles.header}>
+                <Text style={globalStyles.headerTitle}>Регистрация</Text>
+              </View>
+              <View>
+                <Text style={globalStyles.inputTitle} onFocus={() => setIsShowKeyboard(true)}>
+                  Email address:
+                </Text>
+                <TextInput
+                  style={globalStyles.input}
+                  // style={[globalStyles.input, isActive && { backgroundColor: 'purple' }]}
+                  textAlign={'center'}
+                  placeholder="Адрес электронной почты"
+                  value={state.email}
+                  onChangeText={value => setState(prevState => ({ ...prevState, email: value }))}
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={globalStyles.inputTitle}>Password:</Text>
+                <TextInput
+                  style={globalStyles.input}
+                  textAlign={'center'}
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  placeholder="Пароль"
+                  value={state.password}
+                  onChangeText={value => setState(prevState => ({ ...prevState, password: value }))}
+                />
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={globalStyles.button}
+                onPress={() => setIsShowKeyboard(false)}
+              >
+                <Text style={globalStyles.buttonTitle}>Зарегистрироваться</Text>
+              </TouchableOpacity>
 
-            <Button
-        title="Уже есть аккаунт? Войти"
-        onPress={() => navigation.navigate('Login')}
-      />
-          </View>
+              <Button
+                title="Уже есть аккаунт? Войти"
+                onPress={() => navigation.navigate('Login')}
+              />
+            </View>
           </KeyboardAvoidingView>
         </ImageBackground>
-      
       </View>
-      </TouchableWithoutFeedback>
-    );
-  }
-
-  
+    </TouchableWithoutFeedback>
+  );
+}
--- config/globalStyles.js
+++ config/globalStyles.js
@@ -1,86 +1,93 @@
-import {
-    StyleSheet
-  } from "react-native";
+import { StyleSheet, Dimensions } from 'react-native';
 
-  // import colors from "../config/colors"
-  import colors from "./colors"
+// import colors from "../config/colors"
+import colors from './colors';
 
-  export const globalStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: colors.primary,
-    },
-    image: {
-      flex: 1,
-      resizeMode: "cover",
-     justifyContent: "flex-end",
-      alignItems: "center",
-    },
-    // imageWhite: {
-    //  flex: 1,
-    //  position: 'absolute',
-    //  justifyContent: "center"
-    // },
-    // imageRectangle: {
-    //   position: 'absolute',
-    //   borderRadius: 6,
-    //   borderColor: colors.secondary
-    // },
-    // imagePlusIcon: {
-    //   width: 36,
-    //   height: 28
-    // },
-    input: {
-      borderWidth: 1,
-      borderRadius: 6,
-      borderColor: colors.secondary,
-      height: 40,
-      color: colors.secondary,
-    },
-    form: {
-    },
-    inputTitle: {
-      color: colors.secondary,
-      marginBottom: 10,
-      fontSize: 18,
-    },
-    button: {
-      height: 40,
-      borderRadius: 6,
-      borderWidth: 1,
-      marginTop: 10,
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#FF6C00",
-      ...Platform.select({
-        ios: {
-          backgroundColor: "transparent",
-          borderColor: colors.secondary,
-          backgroundColor: colors.third
-        },
-        android: {
-          backgroundColor:  "#ffb6c1",
-          borderColor:  "transparent"
-        },
-        default: {
-          // other platforms, web for example
-          backgroundColor: "blue",
-        },
-      }),
-    },
-    buttonTitle: {
-      fontStyle: "normal",
-      fontSize: 18,
-    },
-    header: {
-      fontSize: 25,
-      color: "#212121",
-      alignItems:"center",
-      marginBottom:150
-    }, 
-    headerTitle: {
-      fontSize: 30,
-      color: colors.secondary,
-      fontFamily: 'DMMono-Regular'
-    }
-  });
+const MAX_WIDTH = Dimensions.get('window').width;
+const MAX_HEIGHT = Dimensions.get('window').height;
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    width: MAX_WIDTH,
+    height: MAX_HEIGHT,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  imageWhite: {
+    width: MAX_WIDTH,
+    height: MAX_HEIGHT,
+    position: 'absolute',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageRectangle: {
+    width: MAX_WIDTH,
+    height: MAX_HEIGHT,
+    position: 'absolute',
+    borderRadius: 6,
+    borderColor: colors.secondary,
+  },
+  // imagePlusIcon: {
+  //   width: 36,
+  //   height: 28
+  // },
+  input: {
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: colors.secondary,
+    height: 40,
+    color: colors.secondary,
+  },
+  form: {},
+  inputTitle: {
+    color: colors.secondary,
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  button: {
+    height: 40,
+    borderRadius: 6,
+    borderWidth: 1,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#FF6C00',
+    ...Platform.select({
+      ios: {
+        backgroundColor: 'transparent',
+        borderColor: colors.secondary,
+        backgroundColor: colors.third,
+      },
+      android: {
+        backgroundColor: '#ffb6c1',
+        borderColor: 'transparent',
+      },
+      default: {
+        // other platforms, web for example
+        backgroundColor: 'blue',
+      },
+    }),
+  },
+  buttonTitle: {
+    fontStyle: 'normal',
+    fontSize: 18,
+  },
+  header: {
+    fontSize: 25,
+    color: '#212121',
+    alignItems: 'center',
+    marginBottom: 150,
+  },
+  headerTitle: {
+    fontSize: 30,
+    color: colors.secondary,
+    fontFamily: 'DMMono-Regular',
+  },
+});
