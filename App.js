86d69b0899d15eca9d3fd12938411a6634edf4d9
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-24 19:25:31 +0100

    added useRoute to handle auth and non.auth cases

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import RegistrationScreen from "./Screens/RegistrationScreen"
+import RegistrationScreen from './Screens/RegistrationScreen';
 import LoginScreen from './Screens/LoginScreen';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
@@ -14,18 +14,72 @@ import PostsScreen from './Screens/mainScreen/PostsScreen';
 import CreatePostsScreen from './Screens/mainScreen/CreatePostsScreen';
 import ProfileScreen from './Screens/mainScreen/ProfileScreen';
 
-
 const loadApplication = async () => {
   await Font.loadAsync({
     'DMMono-Regular': require('./assets/fonts/DMMono-Regular.ttf'),
   });
 };
 
-const Stack = createStackNavigator();
+const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
+
+// const useRoute = isAuth => {
+//   if (!isAuth) {
+//     return (
+//       <AuthStack.Navigator>
+//         <AuthStack.Screen
+//           name="Register"
+//           component={RegistrationScreen}
+//           options={{
+//             headerShown: false,
+//           }}
+//         />
+//         <AuthStack.Screen name="Login" component={LoginScreen} />
+//       </AuthStack.Navigator>
+//     );
+//   }
+//   return (
+//     <>
+//       <MainTab.Screen name="Posts" component={PostsScreen} />
+//       <MainTab.Screen name="Create" component={CreatePostsScreen} />
+//       <MainTab.Screen name="Profile" component={ProfileScreen} />
+//     </>
+//   );
+// };
+
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <AuthStack.Navigator>
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Register"
+          component={RegistrationScreen}
+        />
+      </AuthStack.Navigator>
+    );
+  }
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen name="Posts" component={PostsScreen} />
+      <MainTab.Screen name="Create" component={CreatePostsScreen} />
+      <MainTab.Screen name="Profile" component={ProfileScreen} />
+    </MainTab.Navigator>
+  );
+};
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
+  const routing = useRoute(null);
 
   if (!isReady) {
     return (
@@ -40,26 +94,8 @@ export default function App() {
   return (
     <>
       <NavigationContainer>
-    <AppNavigator />
-  </NavigationContainer>
+       {routing}
+      </NavigationContainer>
     </>
   );
 }
-
-// export const AppNavigator = () => (
-//   // <Stack.Navigator initialRouteName="Login">
-//   <Stack.Navigator>
-//     <Stack.Screen name="Register" component={RegistrationScreen}  options={{
-//             headerShown: false,
-//           }}/>
-//      <Stack.Screen name="Login" component={LoginScreen} />
-//   </Stack.Navigator>
-// );
-
-export const AppNavigator = () => (
-  <MainTab.Navigator>
-        <MainTab.Screen name="Posts" component={PostsScreen} />
-        <MainTab.Screen name="Create" component={CreatePostsScreen} />
-        <MainTab.Screen name="Profile" component={ProfileScreen} />
-      </MainTab.Navigator>
-);
