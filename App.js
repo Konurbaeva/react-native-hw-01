f6a598fa8f1a6962c10b6013e90ef7701e933a3f
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-24 19:48:12 +0100

    extracted useRoute to router.js

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-import RegistrationScreen from './Screens/RegistrationScreen';
-import LoginScreen from './Screens/LoginScreen';
+
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import { useState } from 'react';
 
 import 'expo-dev-menu';
-import PostsScreen from './Screens/mainScreen/PostsScreen';
-import CreatePostsScreen from './Screens/mainScreen/CreatePostsScreen';
-import ProfileScreen from './Screens/mainScreen/ProfileScreen';
+
+import { useRoute } from "./router"
 
 const loadApplication = async () => {
   await Font.loadAsync({
     'DMMono-Regular': require('./assets/fonts/DMMono-Regular.ttf'),
   });
 };
-
-const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
 
 // const useRoute = isAuth => {
 //   if (!isAuth) {
@@ -47,39 +39,9 @@ const MainTab = createBottomTabNavigator();
 //   );
 // };
 
-const useRoute = (isAuth) => {
-  if (!isAuth) {
-    return (
-      <AuthStack.Navigator>
-        <AuthStack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Login"
-          component={LoginScreen}
-        />
-        <AuthStack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Register"
-          component={RegistrationScreen}
-        />
-      </AuthStack.Navigator>
-    );
-  }
-  return (
-    <MainTab.Navigator>
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="Create" component={CreatePostsScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
-    </MainTab.Navigator>
-  );
-};
-
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const routing = useRoute(null);
+  const routing = useRoute(true);
 
   if (!isReady) {
     return (
