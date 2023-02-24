777002fbfed88b5005b2bfacdb3acbe2670a50c7
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-24 20:55:11 +0100

    added MaterialIcons

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import RegistrationScreen from './Screens/RegistrationScreen';
 import LoginScreen from './Screens/LoginScreen';
@@ -11,32 +12,40 @@ import ProfileScreen from './Screens/mainScreen/ProfileScreen';
 const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
-export const useRoute = (isAuth) => {
-    if (!isAuth) {
-      return (
-        <AuthStack.Navigator>
-          <AuthStack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Login"
-            component={LoginScreen}
-          />
-          <AuthStack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Register"
-            component={RegistrationScreen}
-          />
-        </AuthStack.Navigator>
-      );
-    }
+export const useRoute = isAuth => {
+  if (!isAuth) {
     return (
-      <MainTab.Navigator>
-        <MainTab.Screen name="Posts" component={PostsScreen} />
-        <MainTab.Screen name="Create" component={CreatePostsScreen} />
-        <MainTab.Screen name="Profile" component={ProfileScreen} />
-      </MainTab.Navigator>
+      <AuthStack.Navigator>
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Register"
+          component={RegistrationScreen}
+        />
+      </AuthStack.Navigator>
     );
-  };
+  }
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialIcons name="post-add" size={24} color="black" />
+          ),
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen name="Create" component={CreatePostsScreen} />
+      <MainTab.Screen name="Profile" component={ProfileScreen} />
+    </MainTab.Navigator>
+  );
+};
