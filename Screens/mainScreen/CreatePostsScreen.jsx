7e522fddd8187f17e13650cd75aa3ac582ddb89b
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-03-07 15:44:52 +0000

    added send button and styling

--- Screens/mainScreen/CreatePostsScreen.jsx
+++ Screens/mainScreen/CreatePostsScreen.jsx
@@ -19,13 +19,18 @@ const CreatePostsScreen = () => {
       <Camera style={styles.camera} ref={setCamera}>
         {photo && (
           <View style={styles.takePhotoContainer}>
-            <Image source={{ uri: photo }} style={{ height: 200, width: 200 }} />
+            <Image source={{ uri: photo }} style={{ height: 200, width: 200, borderRadius: 10 }} />
           </View>
         )}
         <TouchableOpacity onPress={takePhoto} style={styles.snapContainer}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
+      <View>
+      <TouchableOpacity onPress={takePhoto} style={styles.sendBtn}>
+      <Text style={styles.sendLabel}>SEND</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -36,31 +41,44 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
-    height: 300,
-    marginTop: 50,
-    alignItems: 'center',
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginTop: 40
   },
   snap: {
-    color: '#fff',
+    color: "#fff",
   },
   snapContainer: {
-    marginTop: 200,
     borderWidth: 1,
-    borderColor: '#ff0000',
+    borderColor: "#ff0000",
     width: 70,
     height: 70,
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 20,
   },
   takePhotoContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     left: 10,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 1,
   },
+  sendBtn: {
+    marginHorizontal: 30,
+    height: 40,
+    borderWidth: 2,
+    borderColor: "#20b2aa",
+    borderRadius: 10,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sendLabel: {
+    color: "#20b2aa",
+    fontSize: 20,
+  }
 });
 
 export default CreatePostsScreen;
