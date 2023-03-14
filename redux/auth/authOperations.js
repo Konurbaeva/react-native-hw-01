44dd19aa391154cb6a0aa3a9e1980b841a5e5893
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-03-14 02:06:02 +0100

    fixed updateProfile by importing it from firebase/auth

--- redux/auth/authOperations.js
+++ redux/auth/authOperations.js
@@ -1,7 +1,43 @@
 import { app } from '../../firebase/config';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 
 import { authSlice } from './authReducer';
+
+// export const authSignUpUser =
+//   ({ email, password}) =>
+//   async (dispatch, getState) => {
+//     let auth = getAuth();
+//     console.log('AUTH ', auth);
+//     try {
+//       // const user = await createUserWithEmailAndPassword(auth, email, password)
+//       await createUserWithEmailAndPassword(auth, email, password);
+
+//       const user = await auth.currentUser;
+
+//       // user.updateProfile({
+//       //    displayName: email,
+//       // });
+//        user.updateProfile({
+//         displayName: email,
+//        })
+
+//       const { displayName } = user;
+//       console.log('DISPLAYNAME ------------------- ', user.displayName);
+//       console.log('USER ------------------- ', user);
+
+//       const userUpdateProfile = {
+//         userId: user.uid,
+//         nickName: displayName,
+//       };
+
+//       dispatch(authSlice.actions.updateUserProfile(userUpdateProfile));
+
+//       // dispatch(authSlice.actions.updateUserProfile({userId: user.user.uid}))
+//     } catch (error) {
+//       console.log('error', error);
+//       console.log('error.message', error.message);
+//     }
+//   };
 
 export const authSignUpUser =
   ({ email, password}) =>
@@ -14,10 +50,10 @@ export const authSignUpUser =
 
       const user = await auth.currentUser;
 
-      // user.updateProfile({
-      //    displayName: email,
-      // });
-       user.updateProfile({
+      //  user.updateProfile({
+      //   displayName: email,
+      //  })
+      updateProfile({
         displayName: email,
        })
 
@@ -27,7 +63,7 @@ export const authSignUpUser =
 
       const userUpdateProfile = {
         userId: user.uid,
-        nickName: displayName,
+        nickName: user.displayName,
       };
 
       dispatch(authSlice.actions.updateUserProfile(userUpdateProfile));
